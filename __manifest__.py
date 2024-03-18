8fe781829af5a5b18c53a8e81b3afebccf7c13e6
pchieuu <hieup3611@gmail.com>
2024-03-18 22:48:14 +0700

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hospital Management 2',
+    'version': '1.0',
+    'summary': 'Hospital Management',
+    'sequence': -100,
+    'author': 'Phan Chi Hieu',
+    'description': """Hospital Management Software""",
+    'category': 'Productivity',
+    'website': 'https://www.odoomates.tech',
+    'license': 'LGPL-3',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/patient.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
